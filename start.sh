0f966f28e7a2d4ba3a6945a6157ea1451e82cbe7
rafi4152 <awareoaf48@hotmail.com>
2026-07-03 02:03:37 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# Force English language for the script environment
 export USER=root
 export HOME=/root
+export LANG=en_US.UTF-8
 
 # Clean up any old VNC lock files
 rm -rf /tmp/.X* /tmp/.X11-unix
@@ -14,4 +17,5 @@ LISTEN_PORT=${PORT:-8080}
 echo "Starting noVNC on port $LISTEN_PORT..."
 
 # Start websockify to bridge noVNC web interface to VNC
-exec websockify --web /usr/share/novnc $LISTEN_PORT localhost:5901
+# IMPORTANT: Added 0.0.0.0 to properly expose the port on Railway
+exec websockify --web /usr/share/novnc 0.0.0.0:$LISTEN_PORT localhost:5901
